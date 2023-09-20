06fa2c77bf685ba9d82ddd5e6e7a87c56a93d245
Telma Irina <telmairinacoelho@gmail.com>
2023-09-20 14:19:38 +0100

    fixed the potions, not yet randomized

--- js/game.js
+++ js/game.js
@@ -36,7 +36,7 @@ class Game {
 randomizeRecipe(){
 const potionArray = ['images/recipePotion1.png', 'images/recipePotion2.png', 'images/recipePotion3.png', 'images/recipePotion4.png', 'images/recipePotion5.png', 'images/recipePotion6.png', 'images/recipePotion7.png', 'images/recipePotion8.png', 'images/recipePotion9.png', 'images/recipePotion10.png', 'images/recipePotion11.png', 'images/recipePotion12.png', 'images/recipePotion13.png', 'images/recipePotion14.png', 'images/recipePotion15.png', 'images/recipePotion16.png', 'images/recipePotion17.png', 'images/recipePotion18.png', ];
 
-randomImage();
+randomImage()
 function randomImage() {
     const randomIndex = Math.floor(Math.random()*potionArray.length);
     this.recipePotion.src = potionArray[randomIndex];
